424fee05d49cc72f741507ce9c70df3257be79e2
Nebert11 <ngarinebert2020@gmail.com>
2025-10-04 12:34:19 +0300

    New1 commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import ViteSitemapPlugin from "vite-plugin-sitemap";
 
-// https://vitejs.dev/config/
 export default defineConfig(() => ({
   plugins: [
     react(),
+    ViteSitemapPlugin({
+      hostname: "https://travelbuddiessafaris.netlify.app",
+      dynamicRoutes: [
+        "/",          // home
+        "/about",
+        "/contact",
+        "/reviews",
+        "/tours",
+        "/hotels",
+        "/blog",
+      ],
+    }),
   ],
   resolve: {
     alias: {
